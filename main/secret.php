c58ac2086c748515ff020a29d78c7142032c5b9f
Kavya Harlalka <kavyaharlalka@gmail.com>
2023-04-18 00:38:22 -0400

    Final updates to scripts

--- main/secret.php
+++ main/secret.php
@@ -1,5 +1,11 @@
 <?php
 
-// The flag is CTF_SDaT{f1l3_inclu510n_vuln3r4b1l1ty}
+if ( $_SERVER['REQUEST_METHOD']=='GET' && realpath(__FILE__) == realpath( $_SERVER['SCRIPT_FILENAME'] ) ) {
+    header( 'HTTP/1.0 403 Forbidden', TRUE, 403 );
+	exit;
+
+}
+
+echo "f1l3_inclu510n_vuln3r4b1l1ty";
 
 ?>
