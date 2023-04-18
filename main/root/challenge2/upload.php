48d938944a9e08c2476b781106991b9ad0b079eb
Kavya Harlalka <kavyaharlalka@gmail.com>
2023-04-18 00:19:09 -0400

    Update file structure

--- main/root/challenge2/upload.php
+++ main/root/challenge2/upload.php
@@ -1,6 +1,7 @@
 <?php
 
-$target_dir = "uploads/";
+$dir = getcwd();
+$target_dir = $dir . "/uploads/";
 $target_file = $target_dir . basename($_FILES["uploaded_file"]["name"]);
 
 // Limit the allowed file size 
@@ -15,8 +16,8 @@ if (file_exists($target_file)) {
 	exit;
 }
 
-// Move file to the uploads directory
-if (move_uploaded_file($_FILES["uploaded_file"]["tmp_name"], $target_file)) {
+// Copy the file to the uploads directory
+if (copy($_FILES["uploaded_file"]["tmp_name"], $target_file)) {
 	echo "The file ". htmlspecialchars( basename( $_FILES["uploaded_file"]["name"])). " has been successfully uploaded!";
 } else {
 	echo "Unexpected error uploading your file.";
